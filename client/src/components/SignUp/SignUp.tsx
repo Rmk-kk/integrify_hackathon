b3951540a1e99c0369c7414bc37e96a2a6e527b3
Yasser Shalash <yasservanshalash@gmail.com>
2023-01-31 18:32:40 +0100

    passwords dont match

--- client/src/components/SignUp/SignUp.tsx
+++ client/src/components/SignUp/SignUp.tsx
@@ -1,84 +1,110 @@
-import React, { MouseEventHandler, useState } from 'react';
-import { Box } from '@mui/material';
-import './SignUp.scss';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { logIn } from '../../redux/slices/userReducer';
+import React, { MouseEventHandler, useState } from "react";
+import { Box, Typography } from "@mui/material";
+import "./SignUp.scss";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logIn } from "../../redux/slices/userReducer";
 
 type SignUpProps = {
   showSignIn: MouseEventHandler<HTMLParagraphElement>;
   signUp: boolean;
 };
 
-
-
 const SignUp = ({ showSignIn, signUp }: SignUpProps) => {
   // form field states
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-const [confirmPassword, setConfirmPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+  const nav = useNavigate();
+  const dispatch = useDispatch();
+  // form handlers
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
 
-const nav = useNavigate();
-const dispatch = useDispatch();
-// form handlers
-const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  setEmail(event.target.value);
-};
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
 
-const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  setPassword(event.target.value);
-};
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
 
-const handleConfirmPasswordChange = (
-  event: React.ChangeEvent<HTMLInputElement>
-) => {
-  setConfirmPassword(event.target.value);
-};
+  const handleConfirmPasswordChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setConfirmPassword(event.target.value);
+  };
 
-const handleSubmit = async (event: React.SyntheticEvent) => {
-  event.preventDefault();
-  const response = await axios.post(
-    'http://localhost:4000/api/v1/users/register',
-    { email, password }
-  );
-  console.log(response);
-  if(response.data.accessToken !== "") {
-    dispatch(logIn());
-    nav("/")
-  }
-};
+  const handleSubmit = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    if (password === confirmPassword) {
+      const response = await axios.post(
+        "http://localhost:4000/api/v1/users/register",
+        { email, name, password }
+      );
+      console.log(response);
+      if (response.data.accessToken !== "") {
+        dispatch(logIn());
+        nav("/");
+      }
+    } else {
+      setPasswordsMatch(false);
+    }
+  };
   return (
-    <Box className="signup" sx={{ display: signUp ? 'flex' : 'none' }}>
+    <form
+      className="signup"
+      style={{ display: signUp ? "flex" : "none" }}
+      onSubmit={handleSubmit}
+    >
       <h1 className="signup__heading">SIGN UP</h1>
-        <input
-          name="email"
-          type={'email'}
-          className="singup__email"
-          placeholder="Email"
-          onChange={handleEmailChange}
-        />
-        <input
-          name="password"
-          type={'password'}
-          className="signup__password"
-          placeholder="Password"
-          onChange={handlePasswordChange}
-        />
-        <input
-          name="confirmPassword"
-          type={'password'}
-          className="signup__password"
-          placeholder="Confirm Password"
-          onChange={handleConfirmPasswordChange}
-        />
-        <button className="signup__btn" onClick={handleSubmit}>
-          SIGN UP
-        </button>
+      <input
+        name="email"
+        type={"email"}
+        className="singup__email"
+        placeholder="Email"
+        onChange={handleEmailChange}
+        required
+      />
+      <input
+        name="name"
+        type={"text"}
+        className="singup__email"
+        placeholder="Name"
+        onChange={handleNameChange}
+        required
+      />
+      <input
+        name="password"
+        type={"password"}
+        className="signup__password"
+        placeholder="Password"
+        onChange={handlePasswordChange}
+        required
+      />
+      <input
+        name="confirmPassword"
+        type={"password"}
+        className="signup__password"
+        placeholder="Confirm Password"
+        onChange={handleConfirmPasswordChange}
+        required
+      />
+      <Typography
+        color="error"
+        sx={{ display: passwordsMatch ? "none" : "block" }}
+      >
+        Passwords dont match
+      </Typography>
+      <input type="submit" value={"SIGN UP"} className="signup__btn" />
       <p className="already" onClick={showSignIn}>
         Already have an account?
       </p>
-    </Box>
+    </form>
   );
 };
 

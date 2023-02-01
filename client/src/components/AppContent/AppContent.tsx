b21620d54c899cca93963f39cdf95814c468f2df
Yasser Shalash <yasservanshalash@gmail.com>
2023-02-01 13:14:15 +0100

    app content login

--- client/src/components/AppContent/AppContent.tsx
+++ client/src/components/AppContent/AppContent.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from "../Header/Header";
 import {Outlet} from "react-router-dom";
-
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
 import { Box } from '@mui/material';
 import Auth from '../../pages/Auth/Auth';
 
 const AppContent = () => {
-    // const authenticated = useSelector((state: RootState) => state.user.authentication)
-    const authenticated = true;
+    const authenticated = useSelector((state: RootState) => state.user.authentication)
     return(
             <>
                 {authenticated ? <Box>
